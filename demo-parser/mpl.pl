798f74faea2d487af5272cb8e9e4091a9d63b847
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-01-14 14:40:02 +0800

    Perl script to tail a .dmo from the last seen SV_WELCOME. Read from the end.

---
+++ demo-parser/mpl.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use File::Map qw(map_file);
+
+my $filename = $ARGV[0];
+map_file my ($map), $filename;
+my $size = -s $filename;
+print $size."\n";
+# expected: 187236078
+my $pos = $size - 20000;
+sub test_position() {
+    #    print chr(substr($map, $pos, 1));
+    return ord(substr($map, $pos, 1)) eq 1 &&
+        ord(substr($map, $pos + 2, 1)) eq 42 &&
+        ord(substr($map, $pos + 3, 1)) eq 97 &&
+        ord(substr($map, $pos + 4, 1)) eq 99 &&
+        ord(substr($map, $pos + 5, 1)) eq 95;
+};
+
+my $found = 0;
+
+while(!$found && $pos > 0) {
+    if (test_position) {
+        $found = 1;
+    } else { $pos--; }
+}
+$pos -= 12;
+$| = 1;
+while(1) {
+    my $current_size = -s $filename;
+    my $to_read = $current_size - $pos;
+    print substr($map, $pos, $to_read);
+    $pos += $to_read;
+    sleep 1;
+}
